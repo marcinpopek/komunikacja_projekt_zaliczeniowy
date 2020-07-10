80c496b092f0a68155168b0a2e1dbcb068c49697
marcinpopek <programmer@interia.com>
2020-07-10 15:34:01 +0200

    add while loop and send function

--- src/1konsola.c
+++ src/1konsola.c
@@ -5,6 +5,41 @@ int main() {
 
 	signal(SIGINT, breakHandler);
 
+	unsigned int number = 0;
+    while (1)
+    {
+        printf("\n\n\n");
+        displayInfo(INTRO, "#####     1     #####   x=x%10");
+
+        printf("Enter number [0-%u]: ", UINT_MAX);
+        if (scanf("%ld", &number))
+        {
+            char buffer[BUFFER_SIZE];
+            if (!checkRange(number))
+            {
+                displayInfo(ERROR, TXT_ERROR_INPUT_OUT_OF_RANGE);
+                continue;
+            }
+            sprintf(buffer, "%ld", number);
+            displayInfo(INPUT, buffer);
+
+            number = transform(number);
+            if (!checkRange(number))
+            {
+                displayInfo(ERROR, TXT_ERROR_OUTPUT_OUT_OF_RANGE);
+                continue;
+            }
+            sprintf(buffer, "%ld", number);
+            displayInfo(OUTPUT, buffer);
+
+            send((unsigned int)number);
+        }
+        else
+        {
+            scanf("%*s");
+            displayInfo(ERROR, "Incorrect characters");
+        }
+    }
 
 	return 0;
 }
@@ -13,6 +48,14 @@ void breakHandler() {
 
 	printf("\nShutting down...\n");
     exit(0);
+}
+
+void send(const unsigned int number)
+{
+    char *pTxt = NULL;
+    asprintf(&pTxt, "./2argument.out -i %u", number);
+    system(pTxt);
+    free(pTxt);
 }
 
 unsigned int transform(unsigned int number) {
